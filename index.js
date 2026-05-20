17f0827405391161f124c2f2bb8f8cb5f34716e3
Ahmed Elbastawesy <72830394+ahmedadel17@users.noreply.github.com>
2026-05-20 16:17:56 +0300

    adding page size and adding default printer

--- index.js
+++ index.js
@@ -21,12 +21,28 @@ app.on('ready', () => {
 });
 
 // Listen for print requests
+// Listen for print requests
 ipcMain.on('print-html', (event, data) => {
   const printWin = new BrowserWindow({ show: false })
   printWin.loadURL(`data:text/html;charset=utf-8,${encodeURIComponent(data.data)}`)
 
-  printWin.webContents.on('did-finish-load', () => {
-    printWin.webContents.print(data.options, (success, failureReason) => {
+  printWin.webContents.on('did-finish-load', async () => {
+    const requestedName = data.options.deviceName
+    const printers = await printWin.webContents.getPrintersAsync()
+    const defaultPrinter = printers.find(p => p.isDefault)
+    const deviceName = printers.find(p => p.name === requestedName)?.name || defaultPrinter?.name;
+
+   
+
+    printWin.webContents.print({
+        ...data.options,
+        silent: true,
+        deviceName,
+        pageSize: {
+          width: 70000,
+          height: 200000
+        },
+      }, (success, failureReason) => {
         if (!success) console.log('Print failed:', failureReason)
         printWin.close()
       }
